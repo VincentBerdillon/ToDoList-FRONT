4a48cc5b7c5d3bbe707514c9233490604bb6185c
Vincent Berdillon <131454299+VincentBerdillon@users.noreply.github.com>
2024-02-13 17:19:59 +0100

    Delete front/back directory

--- front/back/app/database.js
+++
@@ -1,7 +0,0 @@
- const { Sequelize } = require('sequelize');
-
- const sequelize = new Sequelize(process.env.PG_URL, {
-    dialect: "postgres"
- });
- 
- module.exports = sequelize;
--- front/back/app/models/index.js
+++
@@ -1,3 +0,0 @@
-const Task = require('./Task');
-
-module.exports = { Task };
